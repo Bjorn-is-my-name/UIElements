dcd2e0b872a4e3df9fcd540b124f8c4b1d6269f5
Bjorn-is-my-name <72093834+Bjorn-is-my-name@users.noreply.github.com>
2025-09-17 20:43:42 +0200

    removed old project code

--- include/App.hpp
+++ include/App.hpp
@@ -10,6 +10,7 @@ class App
 private:
 	sf::RenderWindow window;
 	sf::Font font;
+
 	RoundedRectangle r;
 	RoundedRectangle r2;
 	Text t;
@@ -17,31 +18,6 @@ private:
 	Checkbox cb;
 	Container<Text> c;
 	LineLayout<> l;
-
-	// Temp
-	std::vector<unsigned short> lidar_bram;
-	int zoomAngle = 0;
-
-	enum class States
-	{
-		Idle,
-		Normal,
-		Zoomed
-	} state = States::Idle;
-
-	void schoolProjectCode();
-	void initializeFakeLidarData();
-	void handleUserInput();
-	sf::Color getColorByDistance(int distance);
-	unsigned short getLidarData(int angle);
-	void setLidarData(int angle, unsigned short distance);
-	void clearScreen(sf::Color color);
-	void drawPixel(int x, int y, sf::Color color);
-	void drawLine(int x1, int y1, int x2, int y2, sf::Color color);
-	void drawCircle(int x, int y, int r, sf::Color color);
-	void drawCircleBorder(int x, int y, int r, sf::Color color);
-	void drawCircleSliceBorder(int x, int y, int r, float startAngle, float endAngle, sf::Color color);
-	// Temp
 
 	void run();
 	void handleEvents();
